8807b8612bef3ca20180f731d8577ee4dfad0e22
Ronnie Holm <mail@bugfree.dk>
2017-12-26 18:36:50 +0100

    Updated

--- TrustpilotAesChallenge/Go/src/program.go
+++ TrustpilotAesChallenge/Go/src/program.go
@@ -11,7 +11,7 @@ import (
 	_ "net/http/pprof"
 )
 
-func decipher() {
+func decrypt() {
 	cipherText, err := base64.StdEncoding.DecodeString("yptyoDdVBdQtGhgoePppYHnWyugGmy0j81sf3zBeUXEO/LYRw+2XmVa0/v6YiSy9Kj8gMn/gNu2I7dPmfgSEHPUDJpNpiOWmmW1/jw/Pt29Are5tumWmnfkazcAb23xe7B4ruPZVxUEhfn/IrZPNZdr4cQNrHNgEv2ts8gVFuOBU+p792UPy8/mEIhW5ECppxGIb7Yrpg4w7IYNeFtX5d9W4W1t2e+6PcdcjkBK4a8y1cjEtuQ07RpPChOvLcSzlB/Bg7UKntzorRsn+y/d72qD2QxRzcXgbynCNalF7zaT6pEnwKB4i05fTQw6nB7SU1w2/EvCGlfiyR2Ia08mA0GikqegYA6xG/EAGs3ZJ0aQUGt0YZz0P7uBsQKdmCg7jzzEMHyGZDNGTj0F2dOFHLSOTT2/GGSht8eD/Ae7u/xnJj0bGgAKMtNttGFlNyvKpt2vDDT3Orfk6Jk/rD4CIz6O/Tnt0NkJLucHtIyvBYGtQR4+mhbfUELkczeDSxTXGDLaiU3de6tPaa0/vjzizoUbNFdfkIly/HWINdHoO83E=")
 	if err != nil {
 		panic(err)
@@ -42,7 +42,7 @@ func decipher() {
 							decryptor := cipher.NewCBCDecrypter(block, iv)
 							decryptor.CryptBlocks(clearText, cipherText)
 							if bytes.Contains(clearText, token) {
-								fmt.Printf("%v\n%s", k, clearText)
+								fmt.Printf("%d %d %d %d %d %d\n%s", a, b, c, d, e, f, clearText)
 							}
 						}
 					}
@@ -58,5 +58,5 @@ func main() {
 		log.Println(http.ListenAndServe(":8080", nil))
 	}()
 
-	decipher()
+	decrypt()
 }
