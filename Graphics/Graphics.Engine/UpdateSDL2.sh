7f29937f05b913dd906a4a1a0661bd04874fe0e6
Ronnie Holm <mail@bugfree.dk>
2020-03-19 21:02:06 +0100

    Updated

--- Graphics/Graphics.Engine/UpdateSDL2.sh
+++
@@ -1,4 +0,0 @@
-curl https://raw.githubusercontent.com/flibitijibibo/SDL2-CS/master/src/SDL2.cs > vendor/SDL2.cs
-curl https://raw.githubusercontent.com/flibitijibibo/SDL2-CS/master/src/SDL2_image.cs > vendor/SDL2_image.cs
-curl https://raw.githubusercontent.com/flibitijibibo/SDL2-CS/master/src/SDL2_mixer.cs > vendor/SDL2_mixer.cs
-curl https://raw.githubusercontent.com/flibitijibibo/SDL2-CS/master/src/SDL2_ttf.cs > vendor/SDL2_ttf.cs
