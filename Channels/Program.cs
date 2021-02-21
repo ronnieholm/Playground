92a96a35eb4b00d0464f3b97b2cb31dfad8bfab1
Ronnie Holm <mail@bugfree.dk>
2021-02-21 21:14:23 +0100

    Update

--- Channels/Program.cs
+++ Channels/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Threading.Tasks;
 
-// https://deniskyashif.com/csharp-channels-part-1/
-// https://deniskyashif.com/csharp-channels-part-2/
+// Introduction, larger example with splitting and merging channels:
+// https://deniskyashif.com/2019/12/08/csharp-channels-part-1/
+// https://deniskyashif.com/2019/12/11/csharp-channels-part-2/
+// https://deniskyashif.com/2020/01/07/csharp-channels-part-3/
 
 namespace Channels
 {
