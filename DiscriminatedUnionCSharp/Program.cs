39ed24e47b6aa015277bb6378f7fe16f372f9e42
Ronnie Holm <mail@bugfree.dk>
2022-01-08 04:42:26 +0100

    Update

--- DiscriminatedUnionCSharp/Program.cs
+++ DiscriminatedUnionCSharp/Program.cs
@@ -11,33 +11,32 @@ let getArea s =
     | Rectangle(l, b) -> float(l*b)
  */
 
-namespace DiscriminatedUnionCSharp
-{
-    public abstract record Shape
-    {
-        // Case types
-        public record Circle(int Radius) : Shape;
-        public record Rectangle(int Width, int Height) : Shape;
-        
-        // API
-        public static double Area(Shape s) =>
-            s switch {
-                Circle c => Math.PI * c.Radius * c.Radius,
-                Rectangle r => r.Width * r.Height,
-                _ => throw new Exception("Unreachable")
-            };
-    }
+namespace DiscriminatedUnionCSharp;
 
-    static class Program
+public abstract record Shape
+{
+    // Case types
+    public record Circle(int Radius) : Shape;
+    public record Rectangle(int Width, int Height) : Shape;
+        
+    // API
+    public static double Area(Shape s) =>
+        s switch {
+            Circle c => Math.PI * c.Radius * c.Radius,
+            Rectangle r => r.Width * r.Height,
+            _ => throw new Exception("Unreachable")
+        };
+}
+
+static class Program
+{
+    public static void Main()
     {
-        public static void Main()
-        {
-            var circle = new Shape.Circle(5);
-            var rectangle = new Shape.Rectangle(4, 8);
-            var a1 = Shape.Area(circle);
-            var a2 = Shape.Area(rectangle);
-            Console.WriteLine(a1);
-            Console.WriteLine(a2);
-        }
+        var circle = new Shape.Circle(5);
+        var rectangle = new Shape.Rectangle(4, 8);
+        var a1 = Shape.Area(circle);
+        var a2 = Shape.Area(rectangle);
+        Console.WriteLine(a1);
+        Console.WriteLine(a2);
     }
 }
