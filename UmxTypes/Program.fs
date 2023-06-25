9ddb19e4770f458d9157e82da87e89661b2592b4
Ronnie Holm <mail@bugfree.dk>
2023-06-26 00:22:09 +0200

    Add unit of measure value type example

---
+++ UmxTypes/Program.fs
@@ -0,0 +1,23 @@
+﻿open FSharp.UMX
+
+module Name =
+    [<Measure>] type name
+    
+    let create (s: string): Result<string<name>, string> =
+        if s.Length > 10 then
+            Error "Too long"
+        else
+            Ok %s
+        
+    let value (v: string<name>): string =
+        %v
+
+let n = Name.create "Ronnie"
+
+let s =
+    match n with
+    | Ok n -> Name.value n
+    | Error _ -> "Unknown"
+
+// Notice how units of measure is erased at runtime
+printfn $"%A{n} %A{s}"
